060bc5c92c7b17f718a37f410ee1a4bfd9f4a170
Sergey Lepekha <slepekha@Sergeys-MacBook-Pro.local>
2018-01-05 21:41:25 +0200

    initial commit

---
+++ LearnMobileUnity/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Will adjust the camera to follow and face a target
+/// </summary>
+public class CameraBehaviour : MonoBehaviour {
+
+	[Tooltip("What object should camera be looking at")]
+	public Transform target;
+
+	[Tooltip("How offset will the camera be to the target")]
+	public Vector3 offset;
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update () {
+		if (target != null)
+		{
+			transform.position = target.position + offset;
+
+			// Change the rotation to face target
+			transform.LookAt (target);
+		}
+	}
+}
---
+++ LearnMobileUnity/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBehaviour : MonoBehaviour {
+	
+	/// <summary>
+	/// A reference to the Rigidbody component
+	/// </summary>
+	private Rigidbody rb;
+
+	[Tooltip("How fast the ball moves left/right")]
+	public float dodgeSpeed = 5;
+
+	[Tooltip("How fast ball moves automaticaly")]
+	[Range(0, 10)]
+	public float rollSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed;
+		rb.AddForce(horizontalSpeed, 0, rollSpeed);
+	}
+}
