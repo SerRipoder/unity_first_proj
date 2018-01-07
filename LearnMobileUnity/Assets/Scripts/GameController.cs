146bdee661f1cbb230b313868686c48c8fbf02b4
Sergey Lepekha <slepekha@Sergeys-MacBook-Pro.local>
2018-01-07 13:20:08 +0200

    Endless obstacles spawn added

--- LearnMobileUnity/Assets/Scripts/GameController.cs
+++ LearnMobileUnity/Assets/Scripts/GameController.cs
@@ -10,12 +10,18 @@ public class GameController : MonoBehaviour {
 	[Tooltip("A reference to the tile we want to spawn")]
 	public Transform tile;
 
+	[Tooltip("A reference to the obstacle we want to spawn")]
+	public Transform obstacle;
+
 	[Tooltip("Where the first tile should be placed at")]
 	public Vector3 startPoint = new Vector3(0, 0, -5);
 
 	[Tooltip("How many tiles should we create in advance")]
 	[Range(3, 15)]
 	public int initSpawnNum = 10;
+
+	[Tooltip("How many tiles to spawn initially with no obstacles")]
+	public int initNoObstacles = 4;
 
 	/// <summary>
 	/// Where the next tile should be spawned at.
@@ -34,14 +40,14 @@ public class GameController : MonoBehaviour {
 
 		for (int i = 0; i < initSpawnNum; ++i)
 		{
-			SpawnNextTile();
+			SpawnNextTile(i >= initNoObstacles);
 		}
 	}
 
 	/// <summary>
 	/// Will spawn a tile at a certain location and setup the next position
 	/// </summary>
-	public void SpawnNextTile()
+	public void SpawnNextTile(bool spawnObstacles = true)
 	{
 		var newTile = Instantiate(tile, nextTileLocation,
 			nextTileRotation);
@@ -50,6 +56,31 @@ public class GameController : MonoBehaviour {
 		var nextTile = newTile.Find("Next Spawn Point");
 		nextTileLocation = nextTile.position;
 		nextTileRotation = nextTile.rotation;
+
+		if (!spawnObstacles)
+			return;
+
+		var obstaclesSpawnPoints = new List<GameObject>();
+
+		foreach (Transform child in newTile)
+		{
+			if (child.CompareTag("ObstacleSpawn"))
+			{
+				obstaclesSpawnPoints.Add(child.gameObject);
+			}
+		}
+
+		if (obstaclesSpawnPoints.Count > 0)
+		{
+			var spawnPoint = obstaclesSpawnPoints[Random.Range (0, obstaclesSpawnPoints.Count)];
+
+			var spawnPos = spawnPoint.transform.position;
+
+			var newObstacle = Instantiate(obstacle, spawnPos, Quaternion.identity);
+
+			newObstacle.SetParent(spawnPoint.transform);
+		}
+
 	}
 	// Update is called once per frame
 	void Update () {
